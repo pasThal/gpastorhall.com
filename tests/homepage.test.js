cd8a83deea9c5996a0ebfc03f5660e156791fe6e
greg <nslscsuchapter@gmail.com>
2025-07-05 17:59:23 -0400

    test: add homepage test and workflow

---
+++ tests/homepage.test.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const indexPath = path.join(__dirname, '..', 'dist', 'index.html');
+if (!fs.existsSync(indexPath)) {
+  console.error('Homepage not built: ' + indexPath + ' missing');
+  process.exit(1);
+}
+console.log('Homepage exists');
